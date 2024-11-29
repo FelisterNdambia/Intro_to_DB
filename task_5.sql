b6df262eabb55abbd7d5888e7258f429adad09c7
Felister Ndambia <ndambiafelister914@gmail.com>
2024-11-29 13:05:35 +0300

    Corrected something

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
 INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES (3, 'Cole Baidoo', 'Colebaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (3, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
